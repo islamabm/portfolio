cfc0da83ca3f7c76870698870486d1fbd069ba52
islamabm <alo.jalal.55@gmail.com>
2023-11-17 21:06:50 +0200

    setup gh-pages

---
+++ src/assets/index.js
@@ -0,0 +1,88 @@
+import isiLogo from "./isiLogo2.png";
+import LanWanLogo from "./LanWanLogo.jpg";
+import global from "./global.png";
+import Notes from "./Notes.png";
+import wuzzapp from "./wuzzapp.png";
+import weather from "./weather.jpg";
+import bitcoinsR from "./bitcoinsR.png";
+import pdf from "./ISLAM ABO MOKH cv.pdf";
+import musicR from "./musicR.png";
+import musicV from "./musicV.png";
+import mysql from "./mysql.png";
+import sassLogo from "./sassLogo.png";
+import Angular from "./Angular.png";
+import Vue from "./Vue.png";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+
+import meta from "./company/meta.png";
+import shopify from "./company/shopify.png";
+import starbucks from "./company/starbucks.png";
+import tesla from "./company/tesla.png";
+
+import carrent from "./carrent.png";
+import jobit from "./jobit.png";
+import tripguide from "./tripguide.png";
+
+export {
+  isiLogo,
+  Notes,
+  wuzzapp,
+  pdf,
+  Vue,
+  sassLogo,
+  mysql,
+  Angular,
+  bitcoinsR,
+  backend,
+  creator,
+  mobile,
+  LanWanLogo,
+  web,
+  weather,
+  github,
+  menu,
+  close,
+  css,
+  musicR,
+  docker,
+  figma,
+  git,
+  html,
+  javascript,
+  mongodb,
+  global,
+  musicV,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  meta,
+  shopify,
+  starbucks,
+  tesla,
+  carrent,
+  jobit,
+  tripguide,
+};
---
+++ src/constants/index.js
@@ -0,0 +1,368 @@
+import {
+    mobile,
+    backend,
+    Vue,
+    Notes,
+    pdf,
+    sassLogo,
+    wuzzapp,
+    weather,
+    mysql,
+    bitcoinsR,
+    Angular,
+    musicV,
+    musicR,
+    creator,
+    web,
+    javascript,
+    typescript,
+    html,
+    css,
+    reactjs,
+    redux,
+    tailwind,
+    nodejs,
+    mongodb,
+    git,
+    figma,
+    docker,
+    meta,
+    starbucks,
+    LanWanLogo,
+    tesla,
+    shopify,
+    carrent,
+    jobit,
+    tripguide,
+    threejs,
+} from "../assets";
+
+export const navLinks = [
+    {
+        id: "about",
+        title: "About",
+    },
+    {
+        id: "work",
+        title: "Work",
+    },
+    {
+        id: "CV",
+        title: "CV",
+        href: pdf,
+        download: true,
+
+    },
+];
+
+const services = [
+    {
+        title: "Web Developer",
+        icon: web,
+    },
+    {
+        title: "Frontend Developer",
+        icon: mobile,
+    },
+    {
+        title: "Backend Developer",
+        icon: backend,
+    },
+    // {
+    //     title: "Content Creator",
+    //     icon: creator,
+    // },
+];
+
+const technologies = [
+    {
+        name: "HTML 5",
+        icon: html,
+    },
+    {
+        name: "CSS 3",
+        icon: css,
+    },
+    {
+        name: "sass",
+        icon: sassLogo,
+    },
+    {
+        name: "JavaScript",
+        icon: javascript,
+    },
+    {
+        name: "TypeScript",
+        icon: typescript,
+    },
+    {
+        name: "React JS",
+        icon: reactjs,
+    },
+    {
+        name: "Vue JS",
+        icon: Vue,
+    },
+    {
+        name: "Angular",
+        icon: Angular,
+    },
+    {
+        name: "Redux Toolkit",
+        icon: redux,
+    },
+    {
+        name: "Tailwind CSS",
+        icon: tailwind,
+    },
+    {
+        name: "Node JS",
+        icon: nodejs,
+    },
+    {
+        name: "MongoDB",
+        icon: mongodb,
+    },
+    {
+        name: "mysql",
+        icon: mysql,
+    },
+    {
+        name: "Three JS",
+        icon: threejs,
+    },
+    {
+        name: "git",
+        icon: git,
+    },
+
+];
+
+const experiences = [
+    {
+        title: "Full stack developer",
+        company_name: "LanWanIT",
+        icon: LanWanLogo,
+        iconBg: "#383E56",
+        date: "October 2023",
+        points: [
+            "Developing and maintaining web applications using React.js and other related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+    // {
+    //     title: "React Native Developer",
+    //     company_name: "Tesla",
+    //     icon: tesla,
+    //     iconBg: "#E6DEDD",
+    //     date: "Jan 2021 - Feb 2022",
+    //     points: [
+    //         "Developing and maintaining web applications using React.js and other related technologies.",
+    //         "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+    //         "Implementing responsive design and ensuring cross-browser compatibility.",
+    //         "Participating in code reviews and providing constructive feedback to other developers.",
+    //     ],
+    // },
+    // {
+    //     title: "Web Developer",
+    //     company_name: "Shopify",
+    //     icon: shopify,
+    //     iconBg: "#383E56",
+    //     date: "Jan 2022 - Jan 2023",
+    //     points: [
+    //         "Developing and maintaining web applications using React.js and other related technologies.",
+    //         "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+    //         "Implementing responsive design and ensuring cross-browser compatibility.",
+    //         "Participating in code reviews and providing constructive feedback to other developers.",
+    //     ],
+    // },
+    // {
+    //     title: "Full stack Developer",
+    //     company_name: "Meta",
+    //     icon: meta,
+    //     iconBg: "#E6DEDD",
+    //     date: "Jan 2023 - Present",
+    //     points: [
+    //         "Developing and maintaining web applications using React.js and other related technologies.",
+    //         "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+    //         "Implementing responsive design and ensuring cross-browser compatibility.",
+    //         "Participating in code reviews and providing constructive feedback to other developers.",
+    //     ],
+    // },
+];
+
+const testimonials = [
+    {
+        testimonial:
+            "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+        name: "Sara Lee",
+        designation: "CFO",
+        company: "Acme Co",
+        image: "https://randomuser.me/api/portraits/women/4.jpg",
+    },
+    {
+        testimonial:
+            "I've never met a web developer who truly cares about their clients' success like Rick does.",
+        name: "Chris Brown",
+        designation: "COO",
+        company: "DEF Corp",
+        image: "https://randomuser.me/api/portraits/men/5.jpg",
+    },
+    {
+        testimonial:
+            "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+        name: "Lisa Wang",
+        designation: "CTO",
+        company: "456 Enterprises",
+        image: "https://randomuser.me/api/portraits/women/6.jpg",
+    },
+];
+
+const projects = [
+    {
+        name: "Mucikay",
+        description: "This is a Spotify-inspired music streaming web application built with React.js, Redux, React Hooks, Styled-components, MongoDB, Node, HTML, and different APIs such as Youtube API , React DnD, Fast Average Color, etc. It was built by a team of 3 as part of a hackathon",
+        tags: [
+            {
+                name: "react",
+                color: "blue-text-gradient",
+            },
+            {
+                name: "mongodb",
+                color: "green-text-gradient",
+            },
+            {
+                name: "scss",
+                color: "pink-text-gradient",
+            },
+            {
+                name: "node.js",
+                color: "blue-text-gradient",
+            },
+        ],
+        image: musicR,
+        source_code_link: "https://music-k9lb.onrender.com/",
+        github_link: "https://github.com/islamabm/Spotify-react-frontend"
+    },
+    {
+        name: "Mister-Bitcoin",
+        description: "Mister Bitcoin React Contacts Management Application This React application allows users to manage a list of contacts. Users can add, edit, and remove contacts, as well as view details about each contact",
+        tags: [
+            {
+                name: "react",
+                color: "blue-text-gradient",
+            },
+            {
+                name: "mongodb",
+                color: "green-text-gradient",
+            },
+            {
+                name: "scss",
+                color: "pink-text-gradient",
+            },
+
+        ],
+        image: bitcoinsR,
+        source_code_link: "https://islamabm.github.io/Mister-bitcoins-react/#/",
+        github_link: "https://github.com/islamabm/Mister-bitcoins-react"
+    },
+    {
+        name: "Weather App",
+        description: "This React application allows users to fetch and analyze weather data to aid in event planning. The application makes use of OpenAI for advice and suggestions, weather API for weather data and Google Maps for location services.",
+        tags: [
+            {
+                name: "react",
+                color: "green-text-gradient",
+            },
+            {
+                name: "js",
+                color: "blue-text-gradient",
+            },
+
+            {
+                name: "scss",
+                color: "pink-text-gradient",
+            },
+        ],
+        image: weather,
+        source_code_link: "https://islamabm.github.io/weather-app-react/",
+        github_link: "https://github.com/islamabm/weather-app-react",
+    },
+    {
+        name: "Notes App",
+        description: "Note application that allows users to create and manage various types of notes, including image, text, to-do, map, canvas, audio, and video notes.",
+        tags: [
+            {
+                name: "react",
+                color: "green-text-gradient",
+            },
+            {
+                name: "js",
+                color: "blue-text-gradient",
+            },
+
+            {
+                name: "scss",
+                color: "pink-text-gradient",
+            },
+        ],
+        image: Notes,
+        source_code_link: "https://islamabm.github.io/keep-app/",
+        github_link: "https://github.com/islamabm/keep-app"
+    },
+    {
+        name: "wuzzapp",
+        description: "Embark on a seamless messaging experience with our state-of-the-art chat application, meticulously crafted using the robust combination of React, JavaScript, MongoDB, and Node.js. Designed to replicate and enhance the essential features of WhatsApp, our application offers a modern, intuitive, and responsive user interface, ensuring a familiar yet upgraded chat experience.",
+        tags: [
+            {
+                name: "react",
+                color: "green-text-gradient",
+            },
+            {
+                name: "mongodb",
+                color: "blue-text-gradient",
+            },
+
+            {
+                name: "scss",
+                color: "pink-text-gradient",
+            },
+            {
+                name: "node.js",
+                color: "pink-text-gradient",
+            },
+        ],
+        image: wuzzapp,
+        source_code_link: "https://wuzzapp-oslj.onrender.com",
+        github_link: "https://github.com/CrazyDiamond24/WhatsApp-React"
+    },
+    {
+        name: "Muzikay",
+        description: "This is a Spotify-inspired music streaming web application built with Vue.js, Vite, Vuex, SCSS, MongoDB, Node, HTML, and different APIs such as Vue3-Youtube , Smooth DnD, Fast Average Color, etc. It was built as a final project for Coding Academy bootcamp by a team of 3 as part of a hackathon.",
+        tags: [
+            {
+                name: "vue",
+                color: "blue-text-gradient",
+            },
+            {
+                name: "mongodb",
+                color: "green-text-gradient",
+            },
+            {
+                name: "scss",
+                color: "pink-text-gradient",
+            },
+            {
+                name: "node.js",
+                color: "blue-text-gradient",
+            },
+        ],
+        image: musicV,
+        source_code_link: "https://muzikay.onrender.com/#/station/collection",
+        github_link: "https://github.com/islamabm/Muzikay-sprint4"
+    },
+];
+
+export { services, technologies, experiences, testimonials, projects };
