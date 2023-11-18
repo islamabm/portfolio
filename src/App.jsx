8c839a5e5cddbe6a65fca484acc3affa0010caf9
islamabm <alo.jalal.55@gmail.com>
2023-11-18 07:56:23 +0200

    dsad

--- src/App.jsx
+++ src/App.jsx
@@ -16,10 +16,10 @@ const App = () => {
         <Tech />
         <Works />
         {/* <Feedbacks /> */}
-        <div className='relative z-0'>
+        {/* <div className='relative z-0'>
           <Contact />
           <StarsCanvas />
-        </div>
+        </div> */}
       </div>
     </BrowserRouter>
   );
